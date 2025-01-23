a417dc1187f1fb4f3f41108a4e46fd460d224fff
nipodk <nikitapodkopaevco@gmail.com>
2025-01-23 17:44:00 +0200

    POTMS-2 make header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
