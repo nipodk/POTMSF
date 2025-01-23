ca183dc7c8c5be2962fe93c349b0293b5e4cc55d
nipodk <nikitapodkopaevco@gmail.com>
2025-01-23 19:24:49 +0200

    POTMS-3 make login page * fix problem with router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    RouterLink
   ],
   standalone: true,
   templateUrl: './header.component.html',
