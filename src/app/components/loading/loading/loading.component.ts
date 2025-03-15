dae4e5e7a231983433e4fb15b328a688627f385d
nipodk <nikitapodkopaevco@gmail.com>
2025-03-15 20:14:46 +0200

    POTMS-13 add a loading icon * create a loading element

---
+++ src/app/components/loading/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  imports: [],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.css',
+  standalone: true
+})
+export class LoadingComponent {
+
+}
